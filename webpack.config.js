3c4bca07b1e401442aef89baaf64aedb1967166b
lidenghui <denghui.li@thoughtworks.com>
2020-08-10 23:53:53 +0800

    创建library

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: 'Caching',
+        }),
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        // chunkFilename: '[name].chunk.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+};
